bdf32387d7f10743aef33590d1275cc1ce53fc74
pseuder <iop890520@gmail.com>
2023-01-29 12:06:10 +0800

    加入joi驗證資料

--- routes/activity.js
+++ routes/activity.js
@@ -12,14 +12,15 @@ router.post("/create", async (req, res) => {
     if (!formData) return res.status(400).send("未接收到表單資料！");
 
     const { error } = createActivityValidation(formData);
+
     if (error) return res.status(400).send(error.details[0].message);
   } catch (err) {
     res.status(400).send("伺服器錯誤，新活動建立失敗！");
   }
 
   try {
-    let newActivity = new Activity(formData);
-    await newActivity.save();
+    // let newActivity = new Activity(formData);
+    // await newActivity.save();
     res.status(200).send("新活動建立成功！");
   } catch (err) {
     res.status(400).send("伺服器錯誤，新活動建立失敗！");
--- validation/activity.js
+++ validation/activity.js
@@ -1,28 +1,45 @@
 const Joi = require("joi");
 
-const myJoiType = {
-  STRING1: Joi.string().min(1).required().messages({
-    "string.base": `{{#label}} 應該為文字`,
-    "string.empty": ` {{#label}} 不應為空`,
-    "string.min": `{{#label}} 應大於 {{#limit}}個文字`,
-    "any.required": `{{#label}} 應該有值`,
-  }),
+const myJoiMessage = {
+  "string.base": `{{#label}} 應該為文字`,
+  "string.empty": `{{#label}} 不應為空`,
+  "string.min": `{{#label}} 應大於{{#limit}}個文字`,
+  "array.base": `{{#label}} 應該為陣列`,
+  "array.min": `{{#label}} 應大於{{#limit}}個輸入值`,
+  "array.empty": `{{#label}} 不應為空`,
+  "array.length": `{{#label}} 應為{{#limit}}個輸入值`,
+  "number.base": `{{#label}} 應該為數字`,
+  "number.empty": `{{#label}} 不應為空`,
+  "number.min": `{{#label}} 應大於{{#limit}}`,
+  "any.required": `{{#label}} 應該有值`,
 };
 
 // Register Validation
 const createActivityValidation = (data) => {
   const schema = Joi.object({
-    title: myJoiType.STRING1,
-    object: Joi.array().items(Joi.string()).required(),
-    location: Joi.string().min(1).required(),
-    activity_time: Joi.array().items(Joi.date()).required(),
-    enroll_time: Joi.array().items(Joi.date()).required(),
-    fee: Joi.number().required(),
-    manager: Joi.string().required(),
-    manager_contact: Joi.string().required(),
-    quota: Joi.number().required(),
-    activity_imgs: Joi.array().items(Joi.string()),
-    description: Joi.string().required(),
+    title: Joi.string().required().messages(myJoiMessage),
+    object: Joi.array()
+      .items(Joi.string())
+      .required()
+      .min(1)
+      .messages(myJoiMessage),
+    location: Joi.string().required().messages(myJoiMessage),
+    activity_time: Joi.array()
+      .items(Joi.date())
+      .required()
+      .length(2)
+      .messages(myJoiMessage),
+    enroll_time: Joi.array()
+      .items(Joi.date())
+      .required()
+      .length(2)
+      .messages(myJoiMessage),
+    fee: Joi.number().min(0).required().messages(myJoiMessage),
+    manager: Joi.string().required().messages(myJoiMessage),
+    manager_contact: Joi.string().required().messages(myJoiMessage),
+    quota: Joi.number().min(1).required().messages(myJoiMessage),
+    activity_imgs: Joi.array().items(Joi.string()).messages(myJoiMessage),
+    description: Joi.string().required().messages(myJoiMessage),
   });
 
   return schema.validate(data);
