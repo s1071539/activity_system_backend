6c53e4edbd1daa04fb3a81205a3c3f594654a77e
pseuder <iop890520@gmail.com>
2023-02-23 01:56:41 +0800

    Remove filename field from storage configuration for activity routes.

--- routes/activity.js
+++ routes/activity.js
@@ -32,9 +32,6 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public");
   },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + "-" + Date.now());
-  },
 });
 var upload = multer({ storage: storage });
 router.post("/create", upload.array("files"), async (req, res) => {
